bb101e432d73952392524603490df1b00ebb4051
Pasha <pashahrytsyna@gmail.com>
2021-07-26 14:48:46 +0300

    change task 06 to function style

--- task_06.js
+++ task_06.js
@@ -1,32 +1,26 @@
-let input = prompt("Введите число", 0);
+let input;
 const numbers = [];
 let total = 0;
 
-for (let i = 0; input; i += 1) {
-  if (!isNaN(Number(input))) {
-    numbers.push(Number(input));
-    input = prompt("Введите число", 0);
-  } else {
-    alert("Было введено не число, попробуйте еще раз");
-    input = prompt("Введите число", 0);
+function askNumberToPush() {
+  input = prompt("Введите число", 0);
+
+  if (input) {
+    if (!isNaN(Number(input))) {
+      numbers.push(input);
+      askNumberToPush();
+    } else {
+      alert("Было введено не число, попробуйте еще раз");
+      askNumberToPush();
+    }
   }
+  return;
 }
 
-if (numbers.length > 0) {
-  for (let num of numbers) {
-    total += num;
-  }
-  console.log(`Общая сумма чисел равна ${total}`);
+askNumberToPush();
+
+for (let num of numbers) {
+  total += Number(num);
 }
 
-/*
-Здесь хотел без проверки, но в условии сказано "если массив не пустой"
-т.к. цикл for...of не отработает если массив пустой. 
-
-Альтернативно:
-
-for(let num of numbers) {
-    total += num
-}
-console.log(`Общая сумма чисел равна ${total}`);
-*/
+console.log(total);
