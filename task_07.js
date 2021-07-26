0128ea7d707bf7d818db83654423073fee08cd79
Pasha <pashahrytsyna@gmail.com>
2021-07-26 14:24:54 +0300

    change task_07, fixed undefined

--- task_07.js
+++ task_07.js
@@ -13,17 +13,17 @@ const addLogin = function(allLogins, login) {
 
       if(isLoginUnique(allLogins, login)) {
         allLogins.push(login)
-        return console.log('Логин успешно добавлен!')
+        return 'Логин успешно добавлен!'
       }
 
-      return console.log('Такой логин уже используется!')
+      return 'Такой логин уже используется!'
 
   } 
-  return console.log('Ошибка! Логин должен быть от 4 до 16 символов')
+  return 'Ошибка! Логин должен быть от 4 до 16 символов'
 }
 
 
 console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
 console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4s до 16 символов'
 console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
