b59c664ffdb08d1e2af30507ef216e139f88c31b
Pasha <pashahrytsyna@gmail.com>
2021-07-26 14:21:39 +0300

    change task_07

--- task_07.js
+++ task_07.js
@@ -1,16 +1,12 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123']
 
 const isLoginValid = function(login) {
-  if(login.length >= 4 && login.length <= 16) {
-      return true
-  }
-  return false
+  return login.length >= 4 && login.length <= 16
+  
 }
 const isLoginUnique = function(allLogins, login) {
-  if(!allLogins.includes(login)) {
-    return true
-  }
-  return false
+  return !allLogins.includes(login)
+  
 }
 const addLogin = function(allLogins, login) {
   if(isLoginValid(login)) {
